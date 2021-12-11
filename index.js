d8da69ff69ff86380e929ed8a51273dabaacdb5f
EmilianoGarciaLopez <emilianogarcialopez26@gmail.com>
2021-12-11 18:52:08 -0500

    added tor functionallity

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import puppeteer from "puppeteer";
 
+const POLL_ID = 99999999; // CHANGE THIS TO YOUR POLL ID
+
 const browser = await puppeteer.launch({
   headless: false, // set to false to see browser and test if scipt works
   args: ["--proxy-server=socks5://127.0.0.1:9050"],
 });
 
-async function runVotes() {
+async function runVotes(answerID) {
+
   for (let i = 0; i < 200; i++) {
     const page = await browser.newPage();
-    await page.goto("https://poll.fm/10988464/");
-    await page.evaluate(() => {
-      const hornsteinSelect = document.querySelector("#PDI_answer50502380");
+    await page.goto(`https://poll.fm/${POLL_ID}/`);
+    await page.evaluate(function () {
+      const ANSWER_CHOICE = 9999999; // CHANGE THIS TO YOUR ANSWER CHOICE
+      const answerSelect = document.querySelector(`#PDI_answer${ANSWER_CHOICE}`);
       const voteButton = document.querySelector(".pds-vote-button");
-      hornsteinSelect.click();
+      answerSelect.click();
       voteButton.click();
     });
     await page.close();
