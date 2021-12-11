27611dbbbdff8fdb795e7cadd07abadafc318b5e
EmilianoGarciaLopez <emilianogarcialopez26@gmail.com>
2021-12-11 11:25:24 -0500

    added tor functionallity

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 Run the following commands to use the bot:
 
+you must have the tor package installed
+
+```
+brew install tor
+mv /opt/homebrew/etc/tor/torrc.sample /opt/homebrew/etc/tor/torrc
+brew services start tor
+
+```
+test if tor is working
+```
+curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs
+```
+
+
+
 ```
 git clone https://github.com/EmilianoGarciaLopez/voteScams.git
 cd voteScams
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import puppeteer from "puppeteer";
 
-const browser = await puppeteer.launch({});
+const browser = await puppeteer.launch({
+  headless: true,
+  args: ['--proxy-server=socks5://127.0.0.1:9050']
+});
+
 
 async function runVotes() {
   for (let i = 0; i < 15; i++) {
     const page = await browser.newPage();
-    await page.goto("https://poll.fm/10988464");
+    await page.goto("https://poll.fm/10988464/");
     await page.evaluate(() => {
-      let hornsteinSelect = document.querySelector("#PDI_answer50502380");
-      let voteButton = document.querySelector(".pds-vote-button");
+      const hornsteinSelect = document.querySelector("#PDI_answer50502380");
+      const voteButton = document.querySelector(".pds-vote-button");
       hornsteinSelect.click();
       voteButton.click();
     });
@@ -17,14 +21,14 @@ async function runVotes() {
 }
 
 function sleep(seconds) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < (seconds * 1000));
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < seconds * 1000);
 }
 
 while (true) {
-    await runVotes();
-    sleep(120);
+  await runVotes();
+  sleep(120);
 }
